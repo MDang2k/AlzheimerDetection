1bbd6f6e1a1da97eb21a8302cc6673e4d69e4c7a
Potato <minhdangdoan525@gmail.com>
2021-09-01 10:40:20 +0700

    added images and further changes

--- train.py
+++ train.py
@@ -13,43 +13,44 @@ initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
 
 def run(model):
 
-    best_f1 = 0
-    df = read_data('./data')
+    data = read_data('./data')
 
-
-    muTuple = model
-
-    train_loader = tf.data.Dataset.from_tensor_slices((df[0], df[1]))
-    validation_loader = tf.data.Dataset.from_tensor_slices((df[2], df[3]))
+    train_loader = tf.data.Dataset.from_tensor_slices((data[0], data[1]))
+    validation_loader = tf.data.Dataset.from_tensor_slices((data[2], data[3]))
 
     batch_size = 2
 
     train_dataset = (
-        train_loader.shuffle(len(df[0]))
+        train_loader.shuffle(len(data[0]))
             .batch(batch_size)
             .prefetch(2)
         )
 
     validation_dataset = (
-            validation_loader.shuffle(len(df[2]))
+            validation_loader.shuffle(len(data[2]))
             .batch(batch_size)
             .prefetch(2)
         )
-
 
     data = train_dataset.take(1)
     images, labels = list(data)[0]
     images = images.numpy()
     image = images[0]
     
-
+    #Complie model
     model.compile(
         loss="binary_crossentropy",
         optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
         metrics=["acc"]
     )
-
-        
+    # Define callbacks.
+    checkpoint_path = './model/3d_image_classification.h5'
+    checkpoint_cb = keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path, save_best_only=True
+    )
+    early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15)
+    
+    #Train model
     model.fit(
             train_dataset,
             validation_data=validation_dataset,
@@ -61,3 +62,4 @@ if __name__ == '__main__':
     
     model = get_model()
     run(model)
+    
