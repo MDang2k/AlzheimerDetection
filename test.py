5c8b5aba4fbf86311e1ddad3899ab956d1acdb16
Potato <minhdangdoan525@gmail.com>
2021-09-01 14:55:29 +0700

    lastest changes

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import os
 import numpy as np
-from src.dataset import MRIdataset
-from src.model import get_model
+import joblib
 
+def user_predict(checkpoint_path):
+    # Load best weights.
+    model = joblib.load('./model.pkl')
+    model.load_weights(checkpoint_path)
+    prediction = model.predict(np.expand_dims(x_val[0], axis=0))[0]
+    scores = [1 - prediction[0], prediction[0]]
 
-# Load best weights.
-checkpoint_path = './model/3d_image_classification.h5'
-model.load_weights(checkpoint_path)
-prediction = model.predict(np.expand_dims(x_val[0], axis=0))[0]
-scores = [1 - prediction[0], prediction[0]]
+    class_names = ["normal", "abnormal"]
+    for score, name in zip(scores, class_names):
+        print(
+            "This model is %.2f percent confident that CT scan is %s"
+            % ((100 * score), name)
+        )
 
-class_names = ["normal", "abnormal"]
-for score, name in zip(scores, class_names):
-    print(
-        "This model is %.2f percent confident that CT scan is %s"
-        % ((100 * score), name)
-    )
+if __name__ == '__main__':
+    user_predict('./3d_image_classification.h5')
--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import cProfile
+import joblib
 import matplotlib.pyplot as plt
 from tensorflow import keras
+from tensorflow.keras.callbacks import ModelCheckpoint 
 from src.model import get_model
 from src.dataset import read_data
+
 
 
 initial_learning_rate = 0.0001
@@ -37,29 +40,40 @@ def run(model):
     images = images.numpy()
     image = images[0]
     
+    
     #Complie model
+    initial_learning_rate = 0.0001
+    lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
+    )
+
     model.compile(
         loss="binary_crossentropy",
         optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
         metrics=["acc"]
     )
     # Define callbacks.
-    checkpoint_path = './model/3d_image_classification.h5'
     checkpoint_cb = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path, save_best_only=True
+    "3d_image_classification.h5", save_best_only=True
     )
     early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15)
+
     
     #Train model
     model.fit(
             train_dataset,
             validation_data=validation_dataset,
-            epochs=10,
-            batch_size=20
+            epochs=5,
+            batch_size=20,
+            callbacks=[checkpoint_cb, early_stopping_cb],
         )
+
+    #Save model
+    joblib.dump(model,'model.pkl')
 
 if __name__ == '__main__':
     
     model = get_model()
     run(model)
+    
     
